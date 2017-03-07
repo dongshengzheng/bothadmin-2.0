1da1513558a20e9dc1dc9ae4af8ada0959576456
dongsheng.zheng@windyeel.com <ease4East>
2017-03-07 18:27:35 +0800

    deploy.sh added

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,30 @@
 # 关闭tomcat
 # 删除代码
 # 切换到workspace目录， 执行
-git pull origin master
-mvn clean install
 # move .war文件
 # 执行liquibase
 # 解压
 # 删除 .war
 # cp .properties
 # 启动Tomcat
+
+#!/usr/bin/env bash
+
+## 拉去代码
+
+## tomcat 目录 /root/apache-tomcat-6.0.45/
+
+# 关闭tomcat
+ps -ef |grep tomcat |grep -v grep |awk '{print $2}'|xargs kill 9
+# 删除ROOT 代码
+rm -rf /root/apache-tomcat-6.0.45/webapps/bothadmin/ROOT
+git pull origin master
+mvn clean install
+cd /root/apache-tomcat-6.0.45/webapps/bothadmin
+cp ~/Workspace/bothadmin/target/bothadmin.war  .
+unzip bothadmin.war -d ROOT
+rm bothadmin.war
+cp /root/Workspace/override/boxi.properties /root/apache-tomcat-6.0.45/webapps/boxi/ROOT/WEB-INF/classes/jeeplus.properties
+sh /root/apache-tomcat-6.0.45/bin/startup.sh
 
 
